5b6598e4e2bf494f945adef42dc57cb7ee263f8c
than <than@ecca31ef-c969-4b5c-b3cb-57a2555f9ab3>
2012-12-18 02:59:30 +0000

    reset from 5rooms -trunk
    git-svn-id: http://192.168.1.3:8088/usr/local/svn/36b/beta/system/config@3 ecca31ef-c969-4b5c-b3cb-57a2555f9ab3

---
+++ ador_ftp.php
@@ -0,0 +1,7 @@
+<?php
+$_['config_ador_ftp_hostname'] = 'ftp.5rooms.com';
+$_['config_ador_ftp_username'] = 'eftiepie';
+$_['config_ador_ftp_password'] = 'p13t1E3f';
+$_['config_ador_ftp_port'] = 21;
+$_['config_ador_ftp_passive'] = true;
+$_['config_ador_ftp_debug'] = false;
